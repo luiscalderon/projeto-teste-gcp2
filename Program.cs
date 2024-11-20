a1ca8d431f0d1c59b778ae83dede4f581035c725
luis.calderon <lafcalderon@gmail.com>
2024-11-20 09:25:44 -0300

    Teste

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddHttpsRedirection(opt => opt.HttpsPort = 443);
 
 var app = builder.Build();
 
